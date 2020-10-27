d295b2dff52fbbddfb4708bbf3ce11b42b056e93
David Sterry <sterry.david@gmail.com>
2020-10-27 16:25:32 -0700

    update scripts
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,32 @@
 $(document).ready(function() {
   $("form#fortune-survey").submit(function(event) {
     event.preventDefault();
-    let total=0;
+    let question1Total=0;
+    let question2Total=0;
     $("input:checkbox[name=unlucky]:checked").each(function() {
         total += parseInt($(this).val()); 
-        //if numberOfLucky > numberofUnlucky {}
-      //  then #good-luck-hurray;
-      //else if numbefOfUnlucky > numberOfLucky {}
-      //  then #bad-luck-blues;
-      //else if numberOfLucky === numberOfUnlucky {}
-      //  then #try-again-later;
-    });
     $("input:checkbox[name=lucky]:checked").each(function() {
-      total += parseInt($(this).val()); 
-      //if numberOfLucky > numberofUnlucky {}
-      //  then #good-luck-hurray;
-      //else if numbefOfUnlucky > numberOfLucky {}
-      //  then #bad-luck-blues;
-      //else if numberOfLucky === numberOfUnlucky {}
-      //  then #try-again-later;
+        total += parseInt($(this).val()); 
+        if ('total#lucky' > 'total#unlucky') {
+          $('#good-luck-hurray').show();
+        }
+        else if ('total#unlucky' > 'total#lucky') {
+          $('#bad-luck-blues').show();
+        }
+        else if ('total#Lucky' === 'total#Unlucky') {
+          $('#try-again-later').show();
+      }
+    });
+  
     });
   })
   }) 
 
 //Steps 
-//1 get total # of occurences of Q1 & Q2
-//2 take #of  occurences and parseInt
-//3 take parseInt & add # occurences under Q1 & Q2
-//4 if/else if statements
+//done 1 get total # of occurences of Q1 & Q2
+//done 2 take #of  occurences and parseInt
+//done 3 take parseInt & add # occurences under Q1 & Q2
+//done 4 if/else if statements
 //5 results page
 
 
